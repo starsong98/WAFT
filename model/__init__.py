de307c010d025c16c67241642577cd7935241da9
starsong98 <starsong981@gmail.com>
2025-07-21 16:49:43 +0900

    Incremental changes

--- model/__init__.py
+++ model/__init__.py
@@ -1,10 +1,15 @@
 import os
 import sys
 from model.vitwarp_v8 import ViTWarpV8
+# my additions
+from model.vitwarp_fwarp import ViTWarpFWarp
 
 def fetch_model(args):
     if args.algorithm == 'vitwarp':
         model = ViTWarpV8(args)
+    # my own addition(s)
+    elif args.algorithm == 'vitwarp_fwarp':
+        model = ViTWarpFWarp(args)
     else:
         raise ValueError("Unknown algorithm: {}".format(args.algorithm))
     return model
